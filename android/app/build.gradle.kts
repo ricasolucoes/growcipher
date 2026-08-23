d3cc9dba7a9a11c6315fa3f0896512ee4765d503
Ricardo Sierra <sierra.csi@gmail.com>
2026-08-22 21:07:14 -0300

    chore(android): renomeia o applicationId para com.sierratecnologia.growcipher
    O scaffold do `flutter create` deixou o identificador placeholder
    `com.growcipher.growcipher`. O identificador oficial do produto é
    `com.sierratecnologia.growcipher`.
    
    - `applicationId` e `namespace` no build.gradle.kts do módulo app
    - pacote e caminho do MainActivity.kt em android/app/src/main/kotlin
    - bundle identifier de iOS, macOS, Linux e Windows, para que as cinco
      plataformas usem o mesmo identificador
    - copyright de macOS e Windows deixa de citar um domínio inexistente

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.growcipher.growcipher"
+    namespace = "com.sierratecnologia.growcipher"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.growcipher.growcipher"
+        applicationId = "com.sierratecnologia.growcipher"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
